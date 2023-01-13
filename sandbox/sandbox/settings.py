20b40676af86d24fe74bba7810ea9f56347c3854
morskmorsk <khalidfour@yahoo.com>
2023-01-13 00:44:37 -0600

    removing mypy errors

--- sandbox/sandbox/settings.py
+++ sandbox/sandbox/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ SECRET_KEY = \
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
